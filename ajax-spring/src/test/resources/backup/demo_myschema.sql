9b8f619ec60e1b46deadb17523fb1a6fc21d6f42
andrewmist <andrewlina@bellsouth.net>
2014-08-29 13:18:41 -0400

    adding some src/test/resources

---
+++ ajax-spring/src/test/resources/backup/demo_myschema.sql
@@ -0,0 +1,73 @@
+--IF exists DROP SCHEMA MYSCHEMA2 CASCADE;
+---DROP SCHEMA MYSCHEMA if exists CASCADE;
+---DROP SCHEMA MYSCHEMA CASCADE IF EXISTS;
+--DROP TABLE MYSCHEMA.country IF EXISTS;
+--DROP SCHEMA MYSCHEMA IFEXISTS;
+--DROP SCHEMA blax_blax CASCADE IF EXISTS;
+--DROP SCHEMA MYSCHEMA CASCADE;
+
+DROP SCHEMA MYSCHEMA CASCADE;
+----------------DROP SCHEMA MYSCHEMA CASCADE;
+--DROP SCHEMA MYSCHEMA CASCADE IF EXISTS;
+--CREATE SCHEMA MYSCHEMA AUTHORIZATION DBA IF NOT EXISTS;
+--SET AUTHORIZATION DBA;
+--CREATE SCHEMA MYSCHEMA IF NOT EXISTS;
+CREATE SCHEMA MYSCHEMA AUTHORIZATION DBA;
+
+--if SELECT schema_name from INFORMATION_SCHEMA.SHEMATA where shema_name = 'MYSHEMA';
+SET SCHEMA MYSCHEMA;
+
+
+-- County/Provance
+DROP TABLE country IF EXISTS;
+CREATE TABLE country (country_id integer primary key, name varchar(30) not null);
+INSERT into country VALUES (1, 'United States ZZ');
+INSERT into country VALUES (2, 'Russia XX');
+
+
+--DROP TABLE schema1.provance IF EXISTS;
+--CREATE TABLE schema1.provance (country_id int, provance_id int, provance varchar(30) not null);
+
+--INSERT into schema1.provance VALUES (1, 1 ,'Alabama');
+--INSERT into schema1.provance VALUES (1, 2 ,'Washington');
+--INSERT into schema1.provance VALUES (1, 3 ,'KY');
+
+--INSERT into schema1.provance VALUES (2, 1 ,'Kostromskaya Oblast');
+--INSERT into schema1.provance VALUES (2, 2 ,'Kaliningradskaya');
+--INSERT into schema1.provance VALUES (2, 3 ,'Moskovskaya');
+
+--INSERT into schema1.provance VALUES (3, 1 ,'Deli');
+--INSERT into schema1.provance VALUES (3, 2 ,'Mombay');
+--INSERT into schema1.provance VALUES (3, 3 ,'Pakistan?');
+--INSERT into schema1.provance VALUES (3, 4 ,'Boombay');
+
+--DROP TABLE schema1.town IF EXISTS;
+--CREATE TABLE schema1.town (country_id int, provance_id int, town_id int, town_name varchar(30) not null, population int);
+--INSERT into schema1.town VALUES (1, 1, 1, 'Alkatrac', 100);
+--INSERT into schema1.town VALUES (1, 1, 2, 'Area 52', 200);
+
+--INSERT into schema1.town VALUES (1, 2, 1, 'W Town 1', 50);
+--INSERT into schema1.town VALUES (1, 2, 2, 'W Town 2', 150);
+--INSERT into schema1.town VALUES (1, 2, 3, 'W Town 3', 2150);
+
+--INSERT into schema1.town VALUES (1, 3, 1, 'KY 1', 10);
+--INSERT into schema1.town VALUES (1, 3, 2, 'KY 2', 110);
+--INSERT into schema1.town VALUES (1, 3, 3, 'KY 3', 310);
+
+--INSERT into schema1.town VALUES (2, 1, 1, 'Kostroma', 4);
+--INSERT into schema1.town VALUES (2, 1, 2, 'Kaluga', 24);
+
+--INSERT into schema1.town VALUES (2, 2, 1, 'Kaliningrad', 24);
+--INSERT into schema1.town VALUES (2, 2, 2, 'Sovetsk', 214);
+
+--INSERT into schema1.town VALUES (2, 3, 1, 'Moskva', 1224);
+
+--INSERT into schema1.town VALUES (3, 1, 1, 'Deli 1', 1);
+--INSERT into schema1.town VALUES (3, 2, 1, 'Mombay 1', 11);
+--INSERT into schema1.town VALUES (3, 3, 1, 'Pakista 1', 121);
+--INSERT into schema1.town VALUES (3, 4, 1, 'Bomb 1', 34);
+
+--select * from schema1.user u, schema1.address a where u.id =a.user_id;
+--select * from schema1.country;
+
+
